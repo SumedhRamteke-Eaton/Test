d0c3f03e4af734cc8082cbb2e1a066678aa13f1b
Sumedh Ramteke <sumedhsramteke@eaton.com>
2026-08-27 11:49:31 +0530

    feat: Simple Flask app with GitHub Actions workflow

---
+++ flask_app/tests/test_app.py
@@ -0,0 +1,18 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+def test_home_content():
+    client = app.test_client()
+    response = client.get("/")
+
+    assert b"Welcome to the Flask App!" in response.data
